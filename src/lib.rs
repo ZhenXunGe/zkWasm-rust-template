fe825308929a36c36678f286791f5c90846f4880
sinka <xgao@zoyoe.com>
2023-05-24 09:26:24 +1000

    trial one

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,83 @@
 use wasm_bindgen::prelude::*;
-use zkwasm_rust_sdk::Merkle;
+use zkwasm_rust_sdk::{
+    wasm_input,
+    Merkle,
+};
 
 pub fn get_account(account: &[u64;4]) -> [u64; 4] {
     Merkle::get(account[0])
 }
 
+struct VerifyInfo {
+    oldroot: [u64;4],
+    newroot: [u64;4],
+    hash: [u64;4]
+}
+
+impl VerifyInfo {
+    fn read() -> Self {
+        VerifyInfo {
+            oldroot: [
+                unsafe {wasm_input(1)},
+                unsafe {wasm_input(1)},
+                unsafe {wasm_input(1)},
+                unsafe {wasm_input(1)},
+            ],
+            newroot: [
+                unsafe {wasm_input(1)},
+                unsafe {wasm_input(1)},
+                unsafe {wasm_input(1)},
+                unsafe {wasm_input(1)},
+            ],
+            hash : [
+                unsafe {wasm_input(1)},
+                unsafe {wasm_input(1)},
+                unsafe {wasm_input(1)},
+                unsafe {wasm_input(1)},
+            ],
+
+        }
+    }
+}
+
+struct TxInfo {
+    data: [u64;10],
+}
+
+impl TxInfo {
+    fn read() -> Self {
+        TxInfo {
+            data: [
+                unsafe {wasm_input(0)},
+                unsafe {wasm_input(0)},
+                unsafe {wasm_input(0)},
+                unsafe {wasm_input(0)},
+                unsafe {wasm_input(0)},
+                unsafe {wasm_input(0)},
+                unsafe {wasm_input(0)},
+                unsafe {wasm_input(0)},
+                unsafe {wasm_input(0)},
+                unsafe {wasm_input(0)},
+            ]
+        }
+    }
+}
+
 #[wasm_bindgen]
-pub fn zkmain() {
+pub fn test() {
     Merkle::new();
-    let account = get_account(&[0u64; 4]);
+    let verify_info = VerifyInfo::read();
+    let tx_info = TxInfo::read();
+    /*
+    let address = [
+        unsafe {wasm_input(1)},
+        unsafe {wasm_input(1)},
+        unsafe {wasm_input(1)},
+        unsafe {wasm_input(1)},
+    ];
+    let account_info = get_account(&address);
+    */
+
+    // Check hash of tx_info == verify_info.hash
+    //
 }
