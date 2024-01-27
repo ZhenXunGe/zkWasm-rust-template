565d6fd7d413555f85b32b9024ebec63ca2b3363
sinka <xgao@zoyoe.com>
2024-01-27 15:49:09 +1100

    add size for keccak

--- keccak/test.sh
+++ keccak/test.sh
@@ -7,6 +7,6 @@ rm -rf output
 mkdir output
 
 # Single test
-RUST_LOG=info ~/zkWasm/target/release/delphinus-cli  -k 19 --function zkmain --param params --output ./output --wasm ./pkg/output.wasm setup
-RUST_LOG=info ~/zkWasm/target/release/delphinus-cli  -k 19 --function zkmain --param params --output ./output --wasm ./pkg/output.wasm single-prove 
-RUST_LOG=info ~/zkWasm/target/release/delphinus-cli  -k 19 --function zkmain --param params --output ./output --wasm ./pkg/output.wasm single-verify
+RUST_LOG=info ~/zkWasm/target/release/delphinus-cli --host default -k 22 --function zkmain --param params --output ./output --wasm ./pkg/output.wasm setup
+RUST_LOG=info ~/zkWasm/target/release/delphinus-cli --host default -k 22 --function zkmain --param params --output ./output --wasm ./pkg/output.wasm single-prove
+RUST_LOG=info ~/zkWasm/target/release/delphinus-cli --host default -k 22 --function zkmain --param params --output ./output --wasm ./pkg/output.wasm single-verify
